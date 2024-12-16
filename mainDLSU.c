9e97e5bb2a9ef75f129c29e9f12573f8c999ad0c
axvolkzki <amelia_abenoja@dlsu.edu.ph>
2024-12-16 12:02:50 +0800

    Modified: desciption in main file

--- mainDLSU.c
+++ mainDLSU.c
@@ -1,10 +1,24 @@
-//<Preprocessor directives>
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include <time.h>
+/**
+ * Description: This program is titled "Doggos, Ladders, Slides, and U-Turns" (DLSU). 
+ * It is a machine project created as a course requirement for Logic Formulation and Introductory Programming (CCPROG1). 
+ * The game is a modern Command-Line Interface (CLI) turn-based offline board game inspired by the traditional Snakes and Ladders board game. 
+ * Like Snakes and Ladders, it features a board with a 10 x 10 grid of tiles numbered from 1 to 100. 
+ * 
+ * Players participate in a Spartan race with an exciting twist in this board game. 
+ * Instead of the traditional snakes and ladders, the game features dogs, ladders, slides, and U-turns that can appear randomly on different tiles. 
+ * These elements can assist or hinder players navigating the board to reach tile 100. 
+ * The first player to land on this tile is declared the winner.
+ * 
+ * 
+ * Programmed by: Amelia Joyce Abenoja      S12A
+ * Last modified: 16 December 2024
+ * Version: 2.0
+ * 
+ * Acknowledgements:
+ * 
+ */
 
-int main(void) {
+int main() {
 
-    printf("\n\nHope you enjoyed Doggos, Ladders, Slides and U-turns!\nThank you for playing! :)");
+    return 0;
 }
